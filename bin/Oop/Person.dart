d1f18a321acbc679395c73d3543948ffdb2d9545
RivaL-079 <rivalid7890@gmail.com>
2023-10-13 22:50:15 +0800

    New folder for dart dasar

---
+++ bin/Oop/Person.dart
@@ -0,0 +1,18 @@
+class Person {
+  String name = "Saya";
+  String? address;
+
+  // void sayHello(String name) => print("Hello $name, my name is $nama");
+
+  int usia() => 20;
+}
+
+void main() {
+  Person person = Person();
+  person.name = "RivaL";
+  // person.address = "Indonesia";
+
+  print(person.name);
+
+  int usia() => 20;
+}
